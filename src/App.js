3925cfa2e6ffb925da978dcf07d5297204fb4e32
raymartrosali84 <raymartrosali84@gmail.com>
2024-09-06 13:00:09 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import "./css/style.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import SampleFunctionalComponent from './components/SampleFunctionalComponent';
+import SampleClassComponent from './components/SampleClassComponent';
+
+import Navigation from './components/Navigation';
+import Banner from './components/Banner';
+import Body from './components/Body';
+import Footer from './components/Footer';
+
+import { useState, useEffect } from 'react';
+import useFetch from './useFetch';
+import AddProduct from './components/AddProduct';
+
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
 
 function App() {
+  
+
+  let [cart, setCart] = useState([]);
+  let name = "Raymart Rosali";
+  let addItems = (item) => {
+    setCart(prevCart => [...prevCart, item]);
+    alert("Item Added to Cart");
+  }
+
+  const {products, filteredProducts, filterFunction, AddProductFunction} = useFetch("https://kodegoreact.github.io/react-lessons-assets/products.json");
+  
+  // addProduct();
+
+  
+  // https://kodegoreact.github.io/react-lessons-assets/products.json
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <h1>
+        {3+3} 
+        Hello World
+      </h1>
+      <h1>
+        Hello World
+      </h1>
+      <SampleFunctionalComponent></SampleFunctionalComponent>
+      <SampleFunctionalComponent></SampleFunctionalComponent>
+      <SampleFunctionalComponent></SampleFunctionalComponent>
+      <SampleFunctionalComponent />
+
+      <SampleClassComponent></SampleClassComponent>
+      <SampleClassComponent /> */}
+      <BrowserRouter>
+        <Navigation cart={cart}></Navigation>
+        <Banner></Banner>
+        <Routes>
+          <Route path='/' element={<Body addItems={addItems} products={filteredProducts} filterFunction={filterFunction}></Body>}></Route>
+          <Route path='/add' element={<AddProduct AddProductFunction={AddProductFunction}></AddProduct>}></Route>
+        </Routes>
+      </BrowserRouter>
+      <Footer></Footer>
+    </>
+   
   );
 }
 
